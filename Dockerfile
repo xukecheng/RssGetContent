88483f6e363dbf41c0458e4d7270e034fc9bf399
许珂诚 <cnxukecheng@Gmail.com>
2020-11-27 14:28:21 +0800

    使用 Fast Api 框架

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ WORKDIR /app
 COPY . .
 EXPOSE 8999
 RUN pip3 install -r requirements.txt
-CMD python3 ./main.py
+CMD uvicorn test:app --port 8889
--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-import flask, json
-from flask import request
+from typing import Optional
+from fastapi import FastAPI
 import RssGetContent
 
-server = flask.Flask(__name__)
+app = FastAPI()
 
 
-@server.route('/gamersky', methods=['get'])
-def gamersky():
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/gamersky")
+def gamersky(url: str):
     try:
-        url = request.values.get('url')
         return {
             "code": 200,
             "data": RssGetContent.GamerSky(url=url).getContent(),
             "message": "success"
         }
-    except AttributeError:
+    except Exception:
         return {"code": 200, "data": "参数错误", "message": "success"}
-
-
-if __name__ == '__main__':
-    server.run(debug=True, port=8999, host='0.0.0.0')
